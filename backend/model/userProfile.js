5579b6f7e059ab503c78cd83a43f5cac1bf3b63f
“midoriyaShonen441” <khunnatta@gmail.com>
2022-07-12 15:46:20 +0700

    feat: add register api

---
+++ backend/model/userProfile.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose")
+
+const userProfile = new mongoose.Schema(
+    {
+        contact: {
+            email: { type: String, required: true },
+            username: { type: String, required: true },
+            mobile: { type: String, required: true }  
+        },
+        info: {
+            sex: String,
+            maritalStatus: String,
+            occupation: String,
+            interest: String
+        }
+    }, {
+        timestamps: true
+    }
+)
+
+module.exports = mongoose.model("userProfile", userProfile);
