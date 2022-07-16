97095b67f12c7836cedd2d57fe599f90a41d4991
“midoriyaShonen441” <khunnatta@gmail.com>
2022-07-16 17:15:43 +0700

    fix: fix backend maximum file size & fix nginx problem

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const db = require("./connection/connection");
+const bodyParser = require("body-parser");
+
 require("dotenv").config();
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 
 db.connect()
 
