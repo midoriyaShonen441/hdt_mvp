a57d396aa98ddf9353e7c1707221cf9ef6ce7406
pcsishun <pcsishun@gmail.com>
2022-07-21 22:47:19 +0700

    fatch: switch to production frontend

--- frontend/src/APIsetting.js
+++ frontend/src/APIsetting.js
@@ -2,38 +2,39 @@
 ///////////  For Testing  ////////////
 /////////// ++++++++++++++ ////////////
 
-const httpAPI = () => {
-    const isAPI = "http://localhost:8888/backend";
-    return isAPI;
-}
+// const httpAPI = () => {
+//     const isAPI = "http://localhost:8888/backend";
+//     return isAPI;
+// }
 
-const httpAPIWordCloud = () => {
-    const isAPI = "http://127.0.0.1:8773/faskapi"
-    return isAPI;
-}
+// const httpAPIWordCloud = () => {
+//     const isAPI = "http://127.0.0.1:8773/faskapi"
+//     return isAPI;
+// }
 
-const dugAPI = () => {
-    const isDebug = "http://localhost:8888"
-    return isDebug;
-}
+// const dugAPI = () => {
+//     const isDebug = "http://localhost:8888"
+//     return isDebug;
+// }
 
 /////////// ++++++++++++++ ////////////
 /////////// For production ////////////
 ////////// +++++++++++++++ ///////////
 
-// const httpAPI = () => {
-//     const isAPI = "http://localhost:443/backend";
-//     return isAPI;
-// }
+const httpAPI = () => {
+    const isAPI = "http://localhost:443/backend";
+    return isAPI;
+}
 
-// const httpAPIWordCloud = () => {
-//     const isAPI = "http://127.0.0.1:443/faskapi"
-// }
+const httpAPIWordCloud = () => {
+    const isAPI = "http://127.0.0.1:443/faskapi";
+    return isAPI;
+}
 
-// const dugAPI = () => {
-//     const isDebug = "http://localhost:443/backend"
-//     return isDebug;
-// }
+const dugAPI = () => {
+    const isDebug = "http://localhost:443/backend";
+    return isDebug;
+}
 
 /////////// ++++++++++++++ ////////////
 ///////////////////////////////////////
