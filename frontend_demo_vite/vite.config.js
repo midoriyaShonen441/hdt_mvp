08ac133864472b21abdd733e528072a297fe267a
pcsishun <pcsishun@gmail.com>
2022-07-23 11:22:35 +0700

    update

--- frontend_demo_vite/vite.config.js
+++ frontend_demo_vite/vite.config.js
@@ -1,12 +1,12 @@
-import { fileURLToPath, URL } from 'url'
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    host: true,
+  server:{
     port: 3333
   },
   resolve: {
